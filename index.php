28fa09c605be9feea85ab77334fa96d9611143f4
MagnusBengtsson83 <magnus_bengtsson@hotmail.com>
2015-05-19 12:29:08 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <title>Uppsala studentguide</title>
+    <link rel="stylesheet" href="assets/css/stylesheet.css">
+    <script src="assets/js/java.js"></script>
+</head>
+
+<body>
+	<div id="all">
+
+	<div id="header">
+		
+		<div id="leftHeader">
+
+			<ul>
+				<li><a href="#"> Categories </a>	
+				<ul>				
+					<li>Food <input type="checkbox" name="food" value="food" action=""/></li>
+					<li>Transportation <input type="checkbox" name="transportation" value="transportation" action=""/></li>
+					<li>Night life <input type="checkbox" name="nightlife" value="nightlife" action=""/></li>
+				</ul>
+			</li>
+			</ul>
+
+		</div>
+
+		<div id="rightHeader">
+			<h2>Login</h2>
+				<form id="login" name="login" action="login.php" method="POST">
+					Username: <input class="inputs" type="text" name="userName" id="username">
+					Password: <input class="inputs" type="password" name="passWord" id="password">
+					<input class="inputs" type= "submit" value="Login" id="loginButton"> 
+					<p class="notMember">Not a member? Register <a href="reg.php">here!</a></p>
+				</form>
+		</div>
+	</div>
+
+	<div id="body">
+
+				<div id="leftBody">
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN!
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN!
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN!
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN!
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN!
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN!
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN!
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! 
+						HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN! HÄR KOMMER FEEDEN!
+				</div>
+
+				<div id="rightBody">
+					<img class="logo" src="assets/img/logga.png"><br>
+					<div class="copyright">
+						&copy; Copyright Projektgrupp 1<br>Alla rätter serverade
+					</div>
+				</div>
+	</div>
+</div>
+
+</body>	
+</html>
